efc47770229fe58af32f1b9c549af5a2dd19db1f
Harley Trung <harley@bsa.edu.vn>
2017-03-23 12:38:22 +0700

    add search

--- Apps/app.js
+++ Apps/app.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, ListView } from 'react-native';
+import {
+  StyleSheet,
+  Text, View, ListView,
+  Navigator,
+  TextInput,
+} from 'react-native';
 
 const reposURL = 'https://api.github.com/search/repositories';
 
@@ -36,9 +41,9 @@ export default class App extends React.Component {
     return response.json();
   }
 
-  async fetchData() {
+  async fetchData(query = 'react') {
     const json = await this.searchRepos({
-      q: 'react',
+      q: query,
       sort: 'stars',
       desc: 'order',
     });
@@ -46,20 +51,70 @@ export default class App extends React.Component {
     this.setState({dataSource: this.state.dataSource.cloneWithRows(items)});
     console.log('items', items);
   }
-  
+
   _renderRow = (rowData) => {
     return (
       <RepoCell item={rowData} />
-    )
+    );
   }
 
-  render() {
+  _renderScene = (route, navigator) => {
     return (
-      <ListView 
+      <ListView
         style={{paddingTop: 20}}
         enableEmptySections={true}
         dataSource={this.state.dataSource}
         renderRow={this._renderRow}
+      />
+    );
+  }
+
+  _renderSettingButton = () => {
+    return (
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text style={{fontSize: 24}}>Setting</Text>
+      </View>
+    );
+  }
+
+  _applySearch = (value) => {
+    console.log('searching for ' + value);
+    if (value.length == 0) {
+      this.fetchData('react');
+    } else {
+      this.fetchData(value);
+    }
+  }
+
+  _renderSearchField = () => {
+    return (
+      <View style={{flex: 1}}>
+        <TextInput
+          style={{height: 40, width: 300, padding: 8}} placeholder='Search... (e.g. react)'
+          onChangeText={(value) => this._applySearch(value)}
+        />
+      </View>
+    );
+  }
+
+  render() {
+    const defaultRoute = {
+      title: 'Repos',
+    };
+    return (
+      <Navigator initialRoute={defaultRoute}
+        renderScene={this._renderScene}
+        style={{paddingTop: 40}}
+        navigationBar={
+          <Navigator.NavigationBar
+            style = {{flex: 1, flexDirection: 'row', backgroundColor: '#eee'}}
+            routeMapper={{
+              LeftButton: this._renderSearchField,
+              Title: () => null,
+              RightButton: this._renderSettingButton,
+            }}
+          />
+        }
       />
     );
   }
